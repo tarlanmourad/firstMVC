72bd481d5d63b1ed99edb4b9b6332e7d645bf2ef
Tarlan Muradli <terlanmuradli075@mail.ru>
2022-12-26 21:19:19 +0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Bootstrap.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var model = db.Orders.ToList();
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(Orders newOrder)
+        {
+            if (newOrder.OrderID == 0) db.Orders.Add(newOrder);
+
+            else
+            {
+                var updateName = db.Orders.Find(newOrder.OrderID);
+
+                if (updateName == null) return HttpNotFound();
+
+                updateName.ShipName = newOrder.ShipName;
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Update(int id)
+        {
+            var model = db.Orders.Find(id);
+
+            if(model == null) return HttpNotFound();
+
+            return View("New", model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var deleteName = db.Orders.Find(id);
+
+            if(deleteName == null) return HttpNotFound();
+
+            db.Orders.Remove(deleteName);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
